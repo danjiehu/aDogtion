a3c6b1d5f24712eb5045cc5ce10d6beb4975d6fd
Veronika Seroshtan <veronikaseroshtan@Veronikas-MacBook-Air.local>
2021-04-13 23:54:23 +0800

    added home and welcome pages

--- app.js
+++ app.js
@@ -2,11 +2,6 @@
 let config = require('./key')
 App({
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
     wx.BaaS = requirePlugin('sdkPlugin')
     wx.BaaS.wxExtend(wx.login, wx.getUserInfo, wx.requestPayment)
     wx.BaaS.init(config.appKey)
