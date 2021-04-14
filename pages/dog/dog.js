48cb6a45001f0bf044ec90501087f2aedd802c24
Emma Trigo <eetrigo@gmail.com>
2021-04-14 21:03:35 +0800

    fix dog page

---
+++ pages/dog/dog.js
@@ -0,0 +1,19 @@
+// pages/dog/dog.js
+Page({
+  data: {
+    swiperList: ['/images/coco.jpg', '/images/cinnamon.JPG', '/images/bruno.jpg'],
+    activeSwiper: 0
+  },
+
+  onLoad: function (options) {
+
+  },
+
+  swiperChange(e){
+    // console.log("changed", e.detail.current)
+    this.setData({
+      activeSwiper: e.detail.current
+    })
+  },
+
+})
