702df23ec16d4f8227962c9881286cf99d555ee9
Danjie Hu <danjiehu@outlook.com>
2021-04-15 20:34:27 +0800

    added date picker in booking page

--- pages/booking/booking.js
+++ pages/booking/booking.js
@@ -1,65 +1,50 @@
 // pages/booking/booking.js
 Page({
 
-  /**
-   * Page initial data
-   */
   data: {
+
+    selectedDate: null,
+    timeSlots:"",
+    selectedTime:""
 
   },
 
-  /**
-   * Lifecycle function--Called when page load
-   */
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      selectedDate: e.detail.value
+    })
+  },
+
   onLoad: function (options) {
 
   },
 
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
+
   onReady: function () {
 
   },
 
-  /**
-   * Lifecycle function--Called when page show
-   */
   onShow: function () {
 
   },
 
-  /**
-   * Lifecycle function--Called when page hide
-   */
   onHide: function () {
 
   },
 
-  /**
-   * Lifecycle function--Called when page unload
-   */
   onUnload: function () {
 
   },
 
-  /**
-   * Page event handler function--Called when user drop down
-   */
   onPullDownRefresh: function () {
 
   },
 
-  /**
-   * Called when page reach bottom
-   */
   onReachBottom: function () {
 
   },
 
-  /**
-   * Called when user click on the top right corner to share
-   */
   onShareAppMessage: function () {
 
   }
