616cdeffac7479456b7a2ab7cafd7f4c063cc1c4
Emma Trigo <eetrigo@gmail.com>
2021-04-21 22:27:35 +0800

    added filter but not the connection to baas + success sories page laso not finished

--- pages/stories/stories.js
+++ pages/stories/stories.js
@@ -1,41 +1,79 @@
 const app = getApp()
 
 Page({
-
   data: {
     dog: [],
+    dogs: [],
     currentUser: null,
+    canIUseGetUserProfile: false,
+    hasUserInfo: false,
+    swiperList: [],
+    activeSwiper: 0
   },
 
   onLoad: function (options) {
     this.setData({
       currentUser: app.globalData.userInfo
     })
-
     const self = this
     let Dogs = new wx.BaaS.TableObject('adogtion_success_stories')
-    
     Dogs.find().then(
+      (res) => { 
+        self.setData({
+          dogs: res.data.objects.reverse()
+        })
+      }, (err) => {
+        console.log('dogs failed',err)
+      }
+    )
+  },
+  navigateToDog: function (options) {
+    this.setData({dogOn: true})
+    const self = this
+    let Dogs = new wx.BaaS.TableObject
+    ('adogtion_success_stories')
+
+    //  calling one success dog
+    Dogs.get(options.id).then(
       (res) => {
-        console.log('success dog id', res)
+        console.log('dog id', res)
         self.setData ({
           dog: res.data
         })
-        // console.log(res.data)
         let gallery = res.data.image
-        // console.log('gallery', gallery)
+      for (let i = 0; i < gallery.length; i +=1) {
+        let imagePath = gallery[i].path
+        let swiperList = self.data.swiperList
+        swiperList.push(imagePath)
+        self.setData ({
+          swiperList: this.data.swiperList
+        })
+      }
       },
       (err) => {
         console.log('error', err)
       }
     )
+    },
+
+  swiperChange(e){
+    // console.log("changed", e.detail.current)
+    this.setData({
+      activeSwiper: e.detail.current
+    })
   },
 
-  goBack(e) {
-    console.log("back", e)
-    wx.navigateBack({
-      delta: 1,
-    })
-  }
+  
+
+  // goBack(e) {
+  //   console.log("back", e)
+  //   wx.navigateBack({
+  //     delta: 1,
+  //   })
+  // },
+
+  goBack() {
+    this.setData({dogOn: false})
+  },
 
 })
